ab636416dc12d16ab696b234e1c763cd568105c0
samar <semomoamer38@gmail.com>
2025-09-02 22:04:51 +0300

    feat: Implement advanced Blade features in products and about pages

--- resources/views/shop/products.blade.php
+++ resources/views/shop/products.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.app')
 
+@section('title', 'Products')
+
 @section('content')
 <h1>Our Products</h1>
 
-@unless(count($products))
+@unless(empty($products))
+    @foreach($products as $product)
+        <div class="product-item">
+            <h2>{{ $loop->index + 1 }}. {{ $product['name'] }} 
+                @if($loop->first)
+                    <span>(Featured)</span>
+                @endif
+            </h2>
+            <p>{{ $product['description'] }}</p>
+            <p>Price: ${{ $product['price'] }}</p>
+            @if($product['on_sale'])
+                <span class="badge badge-success">Sale</span>
+            @else
+                <span class="badge badge-secondary">Regular</span>
+            @endif
+        </div>
+    @endforeach
+@else
     <p>No products currently available.</p>
 @endunless
-
-@foreach($products as $product)
-    <div>
-        <h2>{{ $loop->index + 1 }}. {{ $product['name'] }}</h2>
-        <p>{{ $product['description'] }}</p>
-        <p>Price: ${{ $product['price'] }}</p>
-
-        @if($product['on_sale'])
-            <span class="badge badge-success">Sale</span>
-        @else
-            <span class="badge badge-secondary">Regular</span>
-        @endif
-
-        @if($loop->first)
-            <p><em>This is our first product!</em></p>
-        @endif
-    </div>
-@endforeach
 @endsection
