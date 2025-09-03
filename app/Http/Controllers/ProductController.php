a7a7cb0399f345c1863e093b524cdcb29bb5a3c4
samar <semomoamer38@gmail.com>
2025-09-03 20:17:49 +0300

    feat: Implement Read (listing & showing singleproduct) functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+public function index()
+{
+    $products = Product::all();
+
+    return view('shop.products', compact('products'));
+}
+
+public function show($id)
+{
+    $product = Product::find($id);
+
+    if (!$product) {
+        return redirect('/products')->with('error', 'Product not found.');
+    }
+
+    return view('shop.product-details', compact('product'));
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\StoreController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', [ShopController::class, 'index'])->name('home');
 
 // صفحة المنتجات
-Route::get('/products', [StoreController::class, 'products'])->name('products');
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
 
 // تفاصيل منتج
 Route::get('/product-details', [StoreController::class, 'productDetails'])->name('product.details');
