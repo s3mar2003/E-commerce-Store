f193b955db20ba1bf5db0ea0500bc8e04c3a016d
samar <semomoamer38@gmail.com>
2025-09-03 20:53:21 +0300

    feat:Implement Update (update routes page) functionality

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@ Route::get('/', [ShopController::class, 'index'])->name('home');
 
 // صفحة المنتجات
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('/products/create', [ProductController::class, 'create']); // لازم تجي قبل show
 Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
 
 Route::get('/product-details', [StoreController::class, 'productDetails'])->name('product.details');
 
