957d85506e2603097ff3fd2774dd14982c07c79f
samar <semomoamer38@gmail.com>
2025-09-06 17:41:14 +0300

    feat: Add reviews table with factories and seeders

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+use App\Models\User;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id' => Product::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
+            'rating' => $this->faker->numberBetween(1, 5),
+            'comment' => $this->faker->sentence(),
+        ];
+    }
+}
