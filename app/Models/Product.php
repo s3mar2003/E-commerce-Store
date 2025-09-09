61cebbe86a1ecee3e426ff2d2d8be5d79776db8e
samar <semomoamer38@gmail.com>
2025-09-09 23:12:06 +0300

    WIP: save local changes before revert

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,14 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Product extends Model
 {
-     use HasFactory; 
     protected $fillable = ['name', 'description', 'price', 'on_sale','image',];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
 }
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+use App\Models\User;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
@@ -17,12 +20,10 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
+            'product_id' => Product::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
             'rating' => $this->faker->numberBetween(1, 5),
-            'comment' => $this->faker->paragraph,
-            'product_id' => \App\Models\Product::factory(),
-            'user_id' => \App\Models\User::factory(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'comment' => $this->faker->sentence(),
         ];
     }
 }
