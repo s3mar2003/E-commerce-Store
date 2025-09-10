73eefaa2d033ffc2559a5b77aee7333e7ac059da
samar <semomoamer38@gmail.com>
2025-09-10 22:53:54 +0300

    feat: Install and configure Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ShopController;
-use App\Http\Controllers\StoreController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\AdminController;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::middleware(['auth', 'can:access-admin-panel'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
-        Route::get('/products', [AdminController::class, 'products'])->name('products.index');
-        Route::get('/categories', [AdminController::class, 'categories'])->name('categories.index');
-    });
-// صفحة المنتجات
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/create', [ProductController::class, 'create']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products/{id}', [ProductController::class, 'show']);
-// Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
-// Route::put('/products/{id}', [ProductController::class, 'update']);
-// Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/contact', function () {
-//     return view('shop.contact');
-// });
-// Route::post('/contact', [ContactController::class, 'submit']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-
-// Route::get('/product-details', [StoreController::class, 'productDetails'])->name('product.details');
-
-// // صفحة "عنّا"
-// Route::get('/about-us', [StoreController::class, 'aboutUs'])->name('about');
+require __DIR__.'/auth.php';
